7f70d6e53199aa40c366ff0a2eaff867936477c2
LilianVital <54815668+LilianVital@users.noreply.github.com>
2019-11-27 17:27:27 -0300

    exericio 7 finalizado

---
+++ R/exercicio7.R
@@ -0,0 +1,16 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see 
+# that the 6th prime is 13.
+# What is the 10001st prime number?
+
+library(numbers)
+
+i <- 0 ##-- contador
+p <- 0 ##-- verifica se o numero é primo até o limite
+
+while(p < 10001){
+  i <- i + 1
+  if(isPrime(i)){
+    p <- p + 1
+  }
+}
+i
